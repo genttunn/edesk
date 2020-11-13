1721882b6b21ccdba4d1d77603887e8f2f19039c
nghitr <genttunn@gmail.com>
2020-11-13 17:33:32 +0200

    fix font size

--- src/items/Notebook.js
+++ src/items/Notebook.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Notebook.css";
-const foo =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam lacus sed vulputate malesuada. Sed eu est ut est viverra laoreet ut quis elit. Sed pharetra lorem ut magna dapibus pretium. Curabitur ut quam vitae arcu lacinia dapibus. Nulla facilisi. Pellentesque interdum ligula molestie metus blandit, in viverra justo feugiat. Maecenas sed lectus volutpat tortor congue porta. Cras pulvinar sit amet neque ac tincidunt. Duis et convallis quam, ut convallis tellus. Maecenas quis diam eros. Donec ut eros malesuada, porttitor libero fermentum, lobortis erat. Cras vel sem cursus, eleifend risus id, tempor arcu. Praesent eu lobortis arcu. ";
 export default function Notebook() {
+  const savedNote = localStorage.getItem("savedNote");
+  const [yesterday, setYesterday] = useState(
+    savedNote === null ? "" : savedNote
+  );
+
+  let save = (e) => {
+    let note = String.valueOf(e.target.value);
+    console.log(Date.now());
+    localStorage.setItem("savedNote", e.target.value);
+  };
+
   return (
     <div className="Notebook">
       <div className="Notebook-left">
         <form>
-          <textarea name="yesterday" id="left" />
+          <textarea name="yesterday" id="left" value={yesterday} />
         </form>
       </div>
       <div className="Notebook-right">
-        <textarea name="today" id="right" />
+        <textarea name="today" id="right" onChange={save} />
       </div>
     </div>
   );
