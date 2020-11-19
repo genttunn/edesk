5a8ab8555abf4365c92726578da33dfa32d44012
nghitr <genttunn@gmail.com>
2020-11-19 12:36:09 +0200

    music player wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
 import "./App.css";
 import Notebook from "./items/Notebook";
-
+import rain from "./assets/rain.mp3";
 function App() {
+  let rainsound = new Audio(rain);
+  rainsound.loop = true;
+  rainsound.onplay = () => {
+    console.log("playing");
+  };
+  rainsound.onpause = () => {
+    console.log("paused");
+  };
+  let startRain = async () => {
+    try {
+      await rainsound.play();
+    } catch (err) {
+      console.log("Failed to play..." + err);
+    }
+  };
+
+  let stopRain = async () => {
+    try {
+      await rainsound.pause();
+    } catch (err) {
+      console.log("Failed to pause..." + err);
+    }
+  };
   return (
     <div className="App">
       <div className="App-container">
         <div className="App-table">
           <Notebook></Notebook>
+          <button onClick={startRain}>Play</button>
+          <button onClick={stopRain}>Pause</button>
         </div>
       </div>
     </div>
@@ -15,6 +40,9 @@ function App() {
 }
 
 export default App;
+{
+  /*  */
+}
 {
   /* <p>Current number is {number}</p>
         <button onClick={addToNumber}>Add 1 to number</button>
---
+++ src/items/MusicPlayer.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import "../styles/MusicPlayer.css";
+export default function MusicPlayer() {
+  return <div></div>;
+}
